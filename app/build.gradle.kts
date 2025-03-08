7f1708f698c0094baa0e68c8aa7aa3a27aa49b49
prashantverma <prashantverma817381@gmail.com>
2025-03-08 16:44:53 +0530

    Added dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,6 +95,8 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.room:room-paging:2.6.1")
 
+    // bottom navigation and other stuff
+    implementation("androidx.compose.material:material:1.6.0")
 }
 
 kapt {
